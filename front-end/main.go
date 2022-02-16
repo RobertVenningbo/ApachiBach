2e0e547137a0b9345322c34f3fc2357c14f3c4f9
phbi <phbi@itu.dk>
2022-02-16 14:57:41 +0100

    Revert "commit"
    This reverts commit d523840a0456003bd233cfa90fd801b74cfc7ade.

--- front-end/main.go
+++ front-end/main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"html/template"
-	"io/ioutil"
 	"log"
 	"net/http"
-	ab "ApachiBach/back-end/crypto.go"
 )
 
 var tpl *template.Template
@@ -34,18 +31,18 @@ func main() {
 	http.HandleFunc("/review", reviewHandler)
 	http.HandleFunc("/log", logHandler)
 	http.HandleFunc("/discussion", discussionHandler)
-	http.HandleFunc("/upload", uploadFile)
 	//http.HandleFunc("/claim", swagHandler)
 	http.ListenAndServe(":80", nil)
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	err := tpl.ExecuteTemplate(w, "home.gohtml", nil)
+	
 	if err != nil {
 		log.Println("LOGGED", err)
 		http.Error(w, "failuree", http.StatusInternalServerError)
 	}
-
+	
 }
 
 func submissionHandler(w http.ResponseWriter, r *http.Request) {
@@ -82,50 +79,4 @@ func logHandler(w http.ResponseWriter, r *http.Request) {
 		log.Println("LOGGED", err)
 		http.Error(w, "failuree", http.StatusInternalServerError)
 	}
-}
-
-func uploadFile(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("File Upload Endpoint Hit")
-	err := tpl.ExecuteTemplate(w, "upload.gohtml", nil)
-
-	if err != nil {
-		log.Println("LOGGED", err)
-		http.Error(w, "failuree", http.StatusInternalServerError)
-	}
-	// Parse our multipart form, 10 << 20 specifies a maximum
-	// upload of 10 MB files.
-	r.ParseMultipartForm(10 << 20)
-	// FormFile returns the first file for the given key `myFile`
-	// it also returns the FileHeader so we can get the Filename,
-	// the Header and the size of the file
-	file, handler, err := r.FormFile("myFile")
-	if err != nil {
-		fmt.Println("Error Retrieving the File")
-		fmt.Println(err)
-		return
-	}
-	defer file.Close()
-	fmt.Printf("Uploaded File: %+v\n", handler.Filename)
-	fmt.Printf("File Size: %+v\n", handler.Size)
-	fmt.Printf("MIME Header: %+v\n", handler.Header)
-	// Create a temporary file within our temp-files directory that follows
-	// a particular naming pattern
-	tempFile, err := ioutil.TempFile("temp-files", "upload-*.pdf")
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer tempFile.Close()
-
-	// read all of the contents of our uploaded file into a
-	// byte array
-	fileBytes, err := ioutil.ReadAll(file)
-	if err != nil {
-		fmt.Println(err)
-	}
-	ab.encryptFile(handler.Filename, fileBytes, "password")
-	// write this byte array to our temporary file
-	tempFile.Write(fileBytes)
-
-	// return that we have successfully uploaded our file!
-	fmt.Fprintf(w, "Successfully Uploaded File\n")
 }
