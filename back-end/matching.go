5f11e1ac301d5c01d6c0230fedf750efcd7899d2
phbi <phbi@itu.dk>
2022-04-20 13:19:47 +0200

    Revert "TestMatchPaper2 tests MatchPaperz"
    This reverts commit 7576abc26efe825903a678cb94d021d87d42a433.

--- back-end/matching.go
+++ back-end/matching.go
@@ -10,9 +10,9 @@ import (
 )
 
 type ReviewSignedStruct struct {
-	Commit *ecdsa.PublicKey
+	Commit ecdsa.PublicKey
 	Keys   []ecdsa.PublicKey
-	Nonce  *big.Int
+	Nonce  big.Int
 }
 
 //step 4
@@ -197,17 +197,15 @@ func (pc *PC) matchPaperz() {
 		}
 
 		reviewStruct := ReviewSignedStruct{
-			commit,
+			*commit,
 			reviewerKeyList,
-			nonce_r,
+			*nonce_r,
 		}
 
 		signature := SignsPossiblyEncrypts(pc.Keys, EncodeToBytes(reviewStruct), "")
 
 		msg := fmt.Sprintf("ReviewSignedStruct with P%v", p.Id)
 		tree.Put(msg, signature)
-
-		pc.supplyNIZK(p)
 	}
 }
 
@@ -237,7 +235,7 @@ func (pc *PC) supplyNIZK(p *Paper) bool {
 
 	submitterPK := pc.GetPaperSubmitterPK(p.Id)
 
-	proof := NewEqProofP256(PaperBigInt, rr, rs, nonce, &submitterPK, &pc.Keys.PublicKey)
+	proof := NewEqProofP256(PaperBigInt, rr, rs, &nonce, &submitterPK, &pc.Keys.PublicKey)
 
 	C1 := Commitment{
 		paperSubmissionCommit.X,
@@ -248,7 +246,7 @@ func (pc *PC) supplyNIZK(p *Paper) bool {
 		reviewCommit.Y,
 	}
 
-	if !proof.OpenP256(&C1, &C2, nonce, &submitterPK, &pc.Keys.PublicKey) {
+	if !proof.OpenP256(&C1, &C2, &nonce, &submitterPK, &pc.Keys.PublicKey) {
 		works = false //for testing
 		fmt.Println("Error: The review commit and paper submission commit does not hide the same paper")
 	} else {
--- back-end/matching_test.go
+++ back-end/matching_test.go
@@ -155,9 +155,9 @@ func TestSupplyNizk(t *testing.T) {
 	ReviewCommit, _ := pc.GetCommitMessagePaperPC(PaperBigInt, rr)
 
 	reviewStruct := ReviewSignedStruct{
-		ReviewCommit,
+		*ReviewCommit,
 		[]ecdsa.PublicKey{reviewer.Keys.PublicKey}, //reviewer doesnt have a key, might propegate this error: "gob: cannot encode nil pointer of type *ecdsa.PublicKey inside interface"
-		nonce,
+		*nonce,
 	}
 
 	signature := SignsPossiblyEncrypts(pc.Keys, EncodeToBytes(reviewStruct), "")
@@ -182,9 +182,9 @@ func TestGetReviewSignedStruct(t *testing.T) {
 	//reviewerKeyList := []ecdsa.PublicKey{}
 
 	reviewStruct := ReviewSignedStruct{
-		commit,
+		*commit,
 		nil,
-		nonce_r,
+		*nonce_r,
 	}
 
 	signature := SignsPossiblyEncrypts(pc.Keys, EncodeToBytes(reviewStruct), "")
@@ -196,16 +196,4 @@ func TestGetReviewSignedStruct(t *testing.T) {
 
 	assert.Equal(t, reviewStruct, r_struct, "TestGetReviewStruct Failed")
 
-}
-
-func TestMatchPapers2(t *testing.T) {
-	submitter.Submit(&p)
-	pc.allPapers = append(pc.allPapers, &p)
-	reviewerSlice := []*Reviewer{&reviewer}
-	reviewer.SignBidAndEncrypt(&p)
-	pc.assignPaper(reviewerSlice)
-	pc.matchPaperz()
-
-
-	
 }
